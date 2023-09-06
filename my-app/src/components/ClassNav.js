fa8d86ef276263ddb84462b70ffd45c7c0b1d243
AdenAshkir <Adenzaken@hotmail.com>
2023-09-06 16:14:19 +0000

    Little setup for challenges

---
+++ my-app/src/components/ClassNav.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class NavbarSimple extends React.Component {
+    constructor(props){
+        super(props):
+        this.state = {
+
+        }
+    }
+}
+
+export default NavbarSimple
